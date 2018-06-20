2a5e3e7acbe90196e78208a9451a5dc1859dafd3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-06-20 18:00:20 +0200

    Add recommendation page for more than 3 requests

--- froide_food/views.py
+++ froide_food/views.py
@@ -1,13 +1,21 @@
 import json
+from datetime import timedelta
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.utils import timezone
 from django.views.decorators.clickjacking import xframe_options_exempt
+
+from froide.foirequest.models import FoiRequest
 
 from .venue_providers import venue_providers, venue_provider
 from .utils import (
     get_hygiene_publicbody, make_request_url, get_city_from_request
 )
+
+
+TIME_PERIOD = timedelta(days=90)
+MAX_REQUEST_COUNT = 3
 
 
 def index(request, base_template='froide_food/base.html', extra_context=None):
@@ -54,4 +62,26 @@ def make_request(request):
         return redirect('food-index')
 
     url = make_request_url(place, pb)
+
+    stopper = False
+
+    if request.user.is_authenticated:
+        request_count = FoiRequest.objects.filter(
+            public_body=pb,
+            user=request.user,
+            first_message__gte=timezone.now() - TIME_PERIOD
+        ).count()
+        if request_count >= MAX_REQUEST_COUNT:
+            stopper = True
+
+    if stopper or request.GET.get('stopper'):
+        return render(request, 'froide_food/recommend.html', {
+            'publicbody': pb,
+            'request_count': request_count,
+            'days': TIME_PERIOD,
+            'max_count': MAX_REQUEST_COUNT,
+            'url': url,
+            'place': place
+        })
+
     return redirect(url)
