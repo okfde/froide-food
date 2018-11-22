9d13b2fbc883a5e0acd0fe9a7852ffc6dbecc6b5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-22 20:18:41 +0100

    Add better admin interface

--- froide_food/admin.py
+++ froide_food/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from froide.foirequest.models import FoiRequest
+
 from .models import VenueRequest, VenueRequestItem
 
 
@@ -12,6 +14,25 @@ class VenueRequestAdmin(admin.ModelAdmin):
     inlines = [
         VenueRequestItemInlineAdmin
     ]
+    date_hierarchy = 'last_request'
+    list_filter = ('last_status', 'last_resolution',)
+    list_display = (
+        'name', 'last_request',
+        'get_last_status_display', 'get_last_resolution_display'
+    )
+    search_fields = ('name', 'ident')
+
+    def get_last_status_display(self, obj):
+        return FoiRequest.STATUS_RESOLUTION_DICT.get(
+            obj.last_status, (obj.last_status,)
+        )[0]
+    get_last_status_display.short_description = 'letzter Status'
+
+    def get_last_resolution_display(self, obj):
+        return FoiRequest.STATUS_RESOLUTION_DICT.get(
+            obj.last_resolution, (obj.last_resolution,)
+        )[0]
+    get_last_resolution_display.short_description = 'letztes Ergebnis'
 
 
 admin.site.register(VenueRequest, VenueRequestAdmin)
