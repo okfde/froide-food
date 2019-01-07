911cab673b6c0f3454ade0ebb8f1f9e005484f19
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-07 16:24:04 +0000

    Use yelp picture size l for thumbnails

--- froide_food/venue_providers/yelp.py
+++ froide_food/venue_providers/yelp.py
@@ -221,7 +221,7 @@ class YelpVenueProvider(BaseVenueProvider):
                 r['location']['city']
             ),
             'city': r['location']['city'],
-            'image': r['image_url'].replace('o.jpg', 'm.jpg'),
+            'image': r['image_url'].replace('o.jpg', 'l.jpg'),
             'rating': r.get('rating'),
             'review_count': r.get('review_count'),
             'url': r['url'],
