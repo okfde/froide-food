e7d9536c7806d05bae1f66ec7cceac898a03bcd9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-08 18:15:32 +0000

    Fix detail fetch city param

--- frontend/javascript/lib/detailmixin.js
+++ frontend/javascript/lib/detailmixin.js
@@ -2,7 +2,7 @@ var FoodDetailMixin = {
   methods: {
     getDetail (data) {
       this.fetching = true
-      window.fetch(`/api/v1/venue/${data.ident}/?lat=${data.lat}&lng=${data.lng}&data=${encodeURIComponent(data.name)}&address=${encodeURIComponent(data.address)}&city=${encodeURIComponent(data.city)}`)
+      window.fetch(`/api/v1/venue/${data.ident}/?lat=${data.lat}&lng=${data.lng}&data=${encodeURIComponent(data.name)}&address=${encodeURIComponent(data.address)}&city=${data.city ? encodeURIComponent(data.city) : ''}`)
         .then((response) => {
           return response.json()
         }).then((data) => {
