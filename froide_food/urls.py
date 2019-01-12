9c3e4e655e25ad81f3cdf82f5351cd0450fe269f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-12 17:40:23 +0100

    Cache serviceworker on server

--- froide_food/urls.py
+++ froide_food/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 from .views import (
     index, embed, make_request
@@ -9,7 +10,7 @@ urlpatterns = [
     path('', index, name='food-index'),
     path('embed/', embed, name='food-embed'),
     path('anfragen/', make_request, name='food-make_request'),
-    path('sw.js', TemplateView.as_view(
+    path('sw.js', cache_page(None)(TemplateView.as_view(
         template_name='froide_food/sw.js',
-        content_type='application/javascript'), name='food-service_worker'),
+        content_type='application/javascript')), name='food-service_worker'),
 ]
