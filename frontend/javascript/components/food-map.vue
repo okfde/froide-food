9b54e5434ab1253d849d4fde7908eb25166ec8b2
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-13 21:17:27 +0100

    Change search placeholder text

--- frontend/javascript/components/food-map.vue
+++ frontend/javascript/components/food-map.vue
@@ -18,7 +18,7 @@
           <div class="searchbar-inner">
             <div class="input-group">
               <div class="clearable-input">
-                <input type="text" v-model="query" class="form-control" placeholder="Suche nach Restaurant etc." @keydown.enter.prevent="userSearch">
+                <input type="text" v-model="query" class="form-control" placeholder="Restaurant, Supermarkt, Kiosk etc." @keydown.enter.prevent="userSearch">
                 <span class="clearer fa fa-close" v-if="query.length > 0" @click="clearSearch"></span>
               </div>
               <div class="input-group-append">
@@ -62,7 +62,7 @@
               <div class="map-search d-none d-md-block">
                 <div class="input-group">
                   <div class="clearable-input">
-                    <input type="text" v-model="query" class="form-control" placeholder="Suche Restaurant"  @keydown.enter.prevent="userSearch">
+                    <input type="text" v-model="query" class="form-control" placeholder="Suche nach Restaurant, Kiosk etc."  @keydown.enter.prevent="userSearch">
                     <span class="clearer fa fa-close" v-if="query.length > 0" @click="clearSearch"></span>
                   </div>
                   <div class="input-group-append">
