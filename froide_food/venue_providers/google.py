772c0672958955a2eacb6c5000ce7d3f4aef549a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-14 14:53:07 +0100

    Improve google venue provider

--- froide_food/venue_providers/google.py
+++ froide_food/venue_providers/google.py
@@ -3,12 +3,13 @@ from django.conf import settings
 import requests
 import logging
 
-from .base import BaseVenueProvider
+from .base import BaseVenueProvider, VenueProviderException
 
 logger = logging.getLogger('froide')
 
-TEXTSEARCH = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+TEXTSEARCH_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
 NEARBY_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+LOOKUP_URL = 'https://maps.googleapis.com/maps/api/place/details/json'
 
 API_KEY = settings.FROIDE_FOOD_CONFIG.get('api_key_google')
 
@@ -23,6 +24,62 @@ RELEVANT_TYPES = {
     'night_club',
 }
 
+ICON_TYPES = {
+    'bakery',
+    'bar',
+    'cafe',
+    'gas_station',
+    'restaurant',
+    'supermarket',
+    'night_club',
+}
+
+FILTERS = [
+    {
+      'name': 'Bäckerei/Konditorei',
+      'icon': 'fa-pie-chart',
+      'active': False,
+      'categories': ['bakery']
+    },
+    {
+      'name': 'Restaurant/Gaststätte',
+      'icon': 'fa-cutlery',
+      'active': False,
+      'categories': ['restaurant']
+    },
+    {
+      'name': 'Café',
+      'icon': 'fa-coffee',
+      'active': False,
+      'categories': ['cafe']
+    },
+    {
+      'name': 'Bar',
+      'icon': 'fa-glass',
+      'active': False,
+      'categories': ['bar']
+    },
+    {
+      'name': 'Diskothek/Club',
+      'icon': 'fa-diamond',
+      'active': False,
+      'categories': ['night_club']
+    },
+    {
+      'name': 'Supermarkt/Discounter',
+      'icon': 'fa-shopping-cart',
+      'active': False,
+      'categories': ['supermarket']
+    },
+    {
+      'name': 'Tankstelle',
+      'icon': 'fa-car',
+      'active': False,
+      'categories': ['gas_station']
+    },
+]
+
+
 GERMANY = (
   47.266667, 5.9,
   55.05, 15.033333
@@ -31,8 +88,8 @@ GERMANY = (
 
 class GoogleVenueProvider(BaseVenueProvider):
     name = 'google'
-    FILTERS = []
-    FIELDS = 'formatted_address,geometry/location,name,permanently_closed,place_id'
+    FILTERS = FILTERS
+    FIELDS = 'formatted_address,geometry/location,name,permanently_closed,place_id,types'
 
     def get_places(self, coordinates=None, location=None, q=None,
                    categories=None, radius=None, **kwargs):
@@ -41,7 +98,7 @@ class GoogleVenueProvider(BaseVenueProvider):
             'language': 'de',
         }
         if location is not None:
-            url = TEXTSEARCH
+            url = TEXTSEARCH_URL
             params.update({
                 'input': location,
                 'fields': 'geometry/location',
@@ -51,7 +108,6 @@ class GoogleVenueProvider(BaseVenueProvider):
             response = requests.get(url, params=params)
             logger.info('API Request: %s', response.request.url)
             candidates = response.json().get('candidates')
-            print(candidates)
             if not candidates:
                 return []
             candidate = candidates[0]
@@ -63,31 +119,67 @@ class GoogleVenueProvider(BaseVenueProvider):
 
         url = NEARBY_URL
         params.update({
-            'location': '%s,%s' % coordinates,
-            'radius': radius
+            'location': '{},{}'.format(*coordinates),
+            'radius': min(radius, 5000),
+            'type': 'restaurant'
         })
         if q is not None:
             params['keyword'] = q
+        else:
+            params['keyword'] = 'Restaurant'
         params['fields'] = self.FIELDS
         response = requests.get(url, params=params)
         logger.info('API Request: %s', response.request.url)
         results = response.json()
-        print(results)
         if 'results' not in results:
             return []
         results = results['results']
 
         return [
-            {
-                'ident': 'google:%s' % r['place_id'],
-                'lat': r['geometry']['location']['lat'],
-                'lng': r['geometry']['location']['lng'],
-                'name': r['name'],
-                'address': r.get('formatted_address') or r.get('vicinity', ''),
-                'image': None,
-                'rating': None,
-            }
+            self.extract_result(r)
             for r in results
             if set(r['types']) & RELEVANT_TYPES
             and not r.get('permanently_closed')
         ]
+
+    def extract_result(self, r):
+        category = list(set(r['types']) & ICON_TYPES)
+        if category:
+            category = category[0]
+        else:
+            category = ''
+        return {
+            'ident': 'google:%s' % r['place_id'],
+            'lat': r['geometry']['location']['lat'],
+            'lng': r['geometry']['location']['lng'],
+            'name': r['name'],
+            'address': r.get('formatted_address') or r.get('vicinity', ''),
+            'category': category
+        }
+
+    def get_place(self, ident):
+        if ident.startswith('google:'):
+            ident = ident.replace('google:', '')
+
+        params = {
+            'key': API_KEY,
+            'language': 'de',
+            'place_id': ident,
+            'fields': self.FIELDS,
+        }
+        response = requests.get(
+            LOOKUP_URL, params=params
+        )
+
+        if response.status_code != 200:
+            logger.warn('API response: %s - %s',
+                        response.status_code, response.text)
+            raise VenueProviderException()
+
+        logger.info('API Request: %s (%s)',
+                    response.request.url, response.status_code)
+        result = response.json()
+        if not result.get('result'):
+            raise VenueProviderException()
+
+        return self.extract_result(result['result'])
