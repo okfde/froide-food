fa7f77997b409558e4a88b3c0d7e106695aa9fc1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-21 14:47:49 +0100

    Add user export callback

--- froide_food/apps.py
+++ froide_food/apps.py
@@ -1,4 +1,5 @@
-# -*- encoding: utf-8 -*-
+import json
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -13,6 +14,9 @@ class FroideFoodConfig(AppConfig):
             connect_request_status_changed
         )
         from froide.foirequest.models import FoiRequest
+        from froide.account.export import registry
+
+        registry.register(export_user_data)
 
         FoiRequest.request_created.connect(connect_request_object)
         FoiRequest.status_changed.connect(connect_request_status_changed)
@@ -26,3 +30,24 @@ class FroideFoodConfig(AppConfig):
             VenueViewSet,
             basename='venue'
         )
+
+
+def export_user_data(user):
+    from froide.foirequest.models.request import get_absolute_domain_short_url
+    from .models import VenueRequestItem
+
+    venue_requests = (
+        VenueRequestItem.objects
+        .filter(foirequest__user=user)
+        .select_related('venue')
+    )
+    if not venue_requests:
+        return
+    yield ('venue_requests.json', json.dumps([
+        {
+            'venue': v.venue.to_place(),
+            'timestamp': v.timestamp.isoformat(),
+            'request': get_absolute_domain_short_url(v.foirequest_id)
+        }
+        for v in venue_requests]).encode('utf-8')
+    )
