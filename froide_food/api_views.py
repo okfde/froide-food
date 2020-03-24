35ddb6a867db080ee54d96f7b71d083fb5393fb5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-03-24 11:23:59 +0100

    Fix broken count of user requests

--- froide_food/api_views.py
+++ froide_food/api_views.py
@@ -74,7 +74,6 @@ class CreateVenueThrottle(UserRateThrottle):
 
 
 class VenueViewSet(viewsets.ViewSet):
-    authentication_classes = ()
     permission_classes = ()
 
     # @method_decorator(cache_page(60*5))
