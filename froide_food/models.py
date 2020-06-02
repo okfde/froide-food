a74693b3c7ed2ff428d9c599693fc3a7568e660a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-06-02 16:45:50 +0200

    Fix deprecation of u-prefixed gettext functions

--- froide_food/models.py
+++ froide_food/models.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.contrib.postgres.fields import JSONField
 from django.contrib.gis.geos import Point
